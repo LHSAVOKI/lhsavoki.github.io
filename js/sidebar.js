8df071217f097f946fabe4bfb0fbd1f47822e1ee
LHSAVOKI <liuhaier980115@outlook.com>
2021-01-26 09:45:31 +0800

    Site updated: 2021-01-26 09:45:31

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,72 +1,109 @@
-$(function () {
-  $('.toggle-sidebar-info > span').on('click', function () {
-    var toggleText = $(this).attr('data-toggle')
-    $(this).attr('data-toggle', $(this).text())
-    $(this).text(toggleText)
-    changeSideBarInfo()
-  })
-  $('#toggle-sidebar').on('click', function () {
-    if (!isMobile() && $('#sidebar').is(':visible')) {
-      var isOpen = $(this).hasClass('on')
-      isOpen ? $(this).removeClass('on') : $(this).addClass('on')
-      if (isOpen) {
-        $('#page-header').removeClass('open-sidebar')
-        $('body').velocity('stop').velocity({
-          paddingLeft: '0px'
-        }, {
-          duration: 200
-        })
-        $('#sidebar').velocity('stop').velocity({
-          translateX: '0px'
-        }, {
-          duration: 200
-        })
-        $('#toggle-sidebar').velocity('stop').velocity({
-          rotateZ: '0deg',
-          color: '#1F2D3D'
-        }, {
-          duration: 200
-        })
+function toggleSidebarNav() {
+  // toggle sidebar nav and panel
+  const activeTabClass = "sidebar-nav-active";
+  const activePanelClass = "sidebar-panel-active";
+  document.querySelectorAll(".sidebar-nav li").forEach((el) => {
+    el.onclick = function() {
+      if (this.classList.contains(activeTabClass)) {
+        return;
+      }
+      document
+        .querySelector("." + activePanelClass)
+        .classList.remove(activePanelClass);
+      document
+        .querySelector("#" + this.dataset.target)
+        .classList.add(activePanelClass);
+      document
+        .querySelector("." + activeTabClass)
+        .classList.remove(activeTabClass);
+      this.classList.add(activeTabClass);
+    };
+  });
+}
+
+/**
+ * 根据目标激活索引
+ * @param {*} target
+ */
+function activateNavByIndex(target) {
+  if (target.classList.contains("active-current")) return;
+
+  document.querySelectorAll(".post-toc .active").forEach((element) => {
+    element.classList.remove("active", "active-current");
+  });
+  target.classList.add("active", "active-current");
+  let parent = target.parentNode;
+  while (!parent.matches(".post-toc")) {
+    if (parent.matches("li")) parent.classList.add("active");
+    parent = parent.parentNode;
+  }
+}
+
+/**
+ * 监听侧边栏目录
+ */
+function listenSidebarTOC() {
+  const navItems = document.querySelectorAll(".post-toc li");
+  if (!navItems.length) return;
+  const sections = [...navItems].map((element) => {
+    const link = element.querySelector(".toc-link");
+    const target = document.getElementById(
+      decodeURI(link.getAttribute("href")).replace("#", "")
+    );
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      window.scrollTo(0, target.offsetTop + 1);
+    });
+    return target;
+  });
+
+  function findIndex(entries) {
+    let index = 0;
+    let entry = entries[index];
+    if (entry.boundingClientRect.top > 0) {
+      index = sections.indexOf(entry.target);
+      return index === 0 ? 0 : index - 1;
+    }
+    for (; index < entries.length; index++) {
+      if (entries[index].boundingClientRect.top <= 0) {
+        entry = entries[index];
       } else {
-        $('#page-header').addClass('open-sidebar')
-        $('body').velocity('stop').velocity({
-          paddingLeft: '300px'
-        }, {
-          duration: 200
-        })
-        $('#sidebar').velocity('stop').velocity({
-          translateX: '300px'
-        }, {
-          duration: 200
-        })
-        $('#toggle-sidebar').velocity('stop').velocity({
-          rotateZ: '180deg',
-          color: '#99a9bf'
-        }, {
-          duration: 200
-        })
+        return sections.indexOf(entry.target);
       }
     }
-  })
-  function changeSideBarInfo () {
-    if ($('.author-info').is(':visible')) {
-      $('.author-info').velocity('stop')
-        .velocity('transition.slideLeftOut', {
-          duration: 300,
-          complete: function () {
-            $('.sidebar-toc').velocity('stop')
-              .velocity('transition.slideRightIn', { duration: 500 })
-          }
-        })
-    } else {
-      $('.sidebar-toc').velocity('stop')
-        .velocity('transition.slideRightOut', {
-          duration: 300,
-          complete: function () {
-            $('.author-info').velocity('stop')
-              .velocity('transition.slideLeftIn', { duration: 500 })
-          }
-        })
-    }
+    return sections.indexOf(entry.target);
   }
-})
+
+  function createIntersectionObserver(marginTop) {
+    marginTop = Math.floor(marginTop + 10000);
+    let intersectionObserver = new IntersectionObserver(
+      (entries, observe) => {
+        let scrollHeight = document.documentElement.scrollHeight + 100;
+        if (scrollHeight > marginTop) {
+          observe.disconnect();
+          createIntersectionObserver(scrollHeight);
+          return;
+        }
+        let index = findIndex(entries);
+        activateNavByIndex(navItems[index]);
+      },
+      {
+        rootMargin: marginTop + "px 0px -100% 0px",
+        threshold: 0,
+      }
+    );
+    sections.forEach((element) => {
+      element && intersectionObserver.observe(element);
+    });
+  }
+
+  createIntersectionObserver(document.documentElement.scrollHeight);
+}
+
+function initSidebar() {
+  toggleSidebarNav();
+  listenSidebarTOC();
+}
+
+document.addEventListener("DOMContentLoaded", initSidebar);
+document.addEventListener("pjax:success", initSidebar);
